32f0aa8452311312a384014100f38b1ed5ddc5dd
anas140 <muhammedanas4140@gmail.com>
2016-12-15 23:27:24 +0530

    nothing

--- application/views/emailError.php
+++ application/views/emailError.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>EmailError</title>
+	<?php $this->load->helper('url'); ?>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script type="text/javascript" src="<?php echo base_url().'js/jquery.min.js'?>"></script>
+	<script type="text/javascript" src="<?php echo base_url().'js/bootstrap.js'?>"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/bootstrap.css'?>">
 </head>
 <body>
 Email Error
